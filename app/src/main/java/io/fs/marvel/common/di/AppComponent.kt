601a873b5a8566b4e10603dd0e3a28dd81aa584f
Fatih <fatih@Fatihs-MacBook-Pro.local>
2021-06-13 16:07:51 +0300

    update di impl

---
+++ app/src/main/java/io/fs/marvel/common/di/AppComponent.kt
@@ -0,0 +1,21 @@
+package io.fs.marvel.common.di
+
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import io.fs.marvel.App
+import io.fs.marvel.common.di.module.AppModule
+import io.fs.marvel.common.di.module.ProviderAppModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [
+    AndroidSupportInjectionModule::class,
+    AppModule::class,
+    ProviderAppModule::class
+])
+interface AppComponent: AndroidInjector<App> {
+
+    @Component.Builder
+    abstract class Builder: AndroidInjector.Builder<App>()
+}
